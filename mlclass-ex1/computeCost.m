ee0a908a6b21cb3d22fc453b69aa9b8bd257ae1e
Kamu <mr.kamu@gmail.com>
2011-11-26 14:40:07 +0800

    fixed the crap cost function.

--- mlclass-ex1/computeCost.m
+++ mlclass-ex1/computeCost.m
@@ -13,9 +13,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-J = 1/(2 * m) * sum(((X * theta)-y).^2);
-
+J = 1 / (2 * m) * (X * theta - y)' * (X * theta - y);
 
 % =========================================================================
 
